d3cd7a707ebe795d35446c9469045f37b3e886a0
rohangawade-2604 <rohangawade2604@gmail.com>
2025-05-14 18:35:35 +0530

    just deleted the file

--- website1/src/Copyright_Compo/Copyright.jsx
+++ website1/src/Copyright_Compo/Copyright.jsx
@@ -3,8 +3,8 @@ import "./Copyright.css"
 export const Copyright = () => {
   return (
     <>
-      <div className="Copyright">
-        <h1 className='copy'>© 2025 1MDM ⚡ by SuperLabs</h1>
+      <div className="Copyright h-[50px]">
+        <h1 className='copy text-center'>© 2025 1MDM ⚡ by SuperLabs</h1>
       </div>
     </>
   )
