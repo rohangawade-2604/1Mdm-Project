1a18583c96a6154c1735a71563e0dbb86edba143
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-27 21:15:24 +0530

    code

--- website1/src/Header_Component/Header.jsx
+++ website1/src/Header_Component/Header.jsx
@@ -1,34 +1,55 @@
-import React from 'react'
-import { useRef } from 'react'
+
 import { Link } from 'react-router-dom'
 import LogoPart from "../assets/Logo.png"
+import { useState } from 'react'
 
 const Header = () => {
-  
+    
+     const [menuOpen, setMenuOpen] = useState(false);
+
+     const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+     }
 
     return (
         <>
-            <section className='flex justify-between mx-25 mt-3 max-sm:mx-0'>
+            <section className='flex justify-between mx-25 mt-3 max-sm:mx-5'>
                 <div className="logo">
                      <img src={LogoPart} alt="" className='' />
                 </div>
            
                 <nav className='mt-4 list-none'>
-                    <ul  style={{maxHeight: "0px"}}>
-                        <li  className='menulist transition-all duration-300 max-sm:flex max-sm:flex-col max-sm:absolute max-sm:top-40 max-sm:left-0 gap-4 max-sm:overflow-hidden max-sm:align-center max-sm:w-full max-sm:bg-white'>
-                        <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>About Us</Link>
-                        <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>Our Story</Link>
-                        <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>Sell on 1MDM</Link>
-                        <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>Pricing</Link>
-                    </li>
-                    </ul>
-                   
-
+                    <ul>
+                        <li  className='menulist gap-4 max-sm:hidden '>
+                            <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>About Us</Link>
+                            <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>Our Story</Link>
+                            <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>Sell on 1MDM</Link>
+                            <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>Pricing</Link>
+                        </li>
+                    </ul>       
                 </nav>
 
-                <div className="hamburg hidden max-sm:block" onClick={showMenu}>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr-cwMeLLj8MfIo3HoXJKFcOFB1g9U4DDMHA&s" alt="" className=' hidden max-sm:block max-sm:w-20 max-sm:h-15 max-sm:cursor-pointer'/>
+                {/* hamburger of the page below the page*/}
+
+                {
+                    menuOpen && (
+                    <ul className='hidden max-sm:flex-col max-sm:block max-sm:absolute max-sm:top-40 max-sm:w-full max-sm: '>
+                       <li className='max-sm:flex max-sm:flex-col max-sm:bg-white max-sm:gap-3'>
+                            <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>About Us</Link>
+                            <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>Our Story</Link>
+                            <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>Sell on 1MDM</Link>
+                            <Link to="/" className='ml-5 transition all duration-100 hover:text-red-600'>Pricing</Link>
+                       </li>
+                    </ul>
+                    )
+                }
+
+
+                {/* hamburger of the page */}
+                <div className="hamburg hidden max-sm:block max-sm:cursor-pointer max-sm:w-15 max-sm:h-10 " onClick={toggleMenu} >
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr-cwMeLLj8MfIo3HoXJKFcOFB1g9U4DDMHA&s" alt="" className=' hidden max-sm:block max-sm:w-15 max-sm:h-15 max-sm:cursor-pointer'/>
                 </div>
+
             </section>
         </>
     )
